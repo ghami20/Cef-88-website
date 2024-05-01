73ee1d87c9d40e260ed8089304ba12de2947a3fe
Gamaliel Quiroz <83394687+ghami20@users.noreply.github.com>
2024-05-01 19:31:30 -0300

    cambio en menu

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-
-
-function mostrar(){
-    document.querySelector('nav').classList.toggle('show');
+function verMenu() {
+  var menunav = document.getElementById("menunav");
+  menunav.classList.toggle("show");
 }
 
-// Obtener todos los enlaces del menú
-const links = document.querySelectorAll('nav a');
 
-// Función para agregar la clase "active" al enlace seleccionado
-function setActiveLink(event) {
-    // Eliminar la clase "active" de todos los enlaces
-    links.forEach(link => link.classList.remove('active'));
-    // Agregar la clase "active" al enlace seleccionado
-    event.target.classList.add('active');
-}
 
-// Escuchar el evento de clic en cada enlace y llamar a la función setActiveLink
-links.forEach(link => {
-    link.addEventListener('click', setActiveLink);
-});
+
